0130db4bc9cfc3d529af1f0e4d6910b17075f178
Muliana Insan Wahyudi <30404686+muliana98@users.noreply.github.com>
2017-10-01 09:09:35 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -23,7 +23,7 @@ else {
 <link rel="stylesheet" href="assets/css/bootstrap.css" type="text/css" media="screen">
 <link rel="stylesheet" href="assets/css/bootstrap-responsive.css" type="text/css" media="screen">
 <link rel="stylesheet" href="assets/css/docs.css" type="text/css" media="screen">
-<link rel="stylesheet" href="assets/css/prettify.css" type="text/css" media="screen">
+<link rel="stylesheet" href="assets/css/pretiffy.css" type="text/css" media="screen">
 <link rel="icon" href="assets/picture/logo_itmedia_flat.png">
 <style>
 
