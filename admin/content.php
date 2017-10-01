c518abeaef820377fabcd6589aa75b022daf16b7
Muliana Insan Wahyudi <30404686+muliana98@users.noreply.github.com>
2017-10-01 10:13:13 +0700

    Update content.php

--- admin/content.php
+++ admin/content.php
@@ -1,4 +1,38 @@
-<p>
-<img src="assets/picture/logo_itmedia_flat_400px.png" width="30%" height="30%"></p>
-<h4>Administrator - Dashboard ITMedia-Com</h4>
-<p>Halaman Dashboard ITMedia-Com</p>
+<?php
+require("koneksi.php");
+$ket_hak_akses = "admin";
+
+$id_akun = mysqli_real_escape_string($koneksi, trim($_SESSION['admin']));
+$seleksi_data = mysqli_query($koneksi, "SELECT * FROM admin WHERE id='$id_akun'");
+$datanya = mysqli_fetch_array($seleksi_data);
+
+	$ket = mysqli_real_escape_string($koneksi, trim($datanya['ket']));
+	
+	if($ket_hak_akses == $ket) {
+		
+
+?>
+
+			<p>
+			<img src="assets/picture/logo_itmedia_flat_400px.png" width="30%" height="30%"></p>
+			<h4>Administrator - Dashboard ITMedia-Com</h4>
+			<p>Halaman Dashboard ITMedia-Com</p>
+
+
+<?php
+
+    }
+	else {
+		
+
+?>
+
+			<p>
+			<img src="assets/picture/logo_itmedia_flat_400px.png" width="30%" height="30%"></p>
+			<h4>Moderator - Dashboard ITMedia-Com</h4>
+			<p>Halaman Dashboard ITMedia-Com</p>
+
+<?php
+	}
+
+?>
