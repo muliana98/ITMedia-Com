afd4ff8bd0d815697f91f00748b94fe7fe1e1735
Muliana Insan Wahyudi <30404686+muliana98@users.noreply.github.com>
2018-08-18 01:25:53 +0700

    Update kategori-tambah.php

--- admin/content/kategori/kategori-tambah.php
+++ admin/content/kategori/kategori-tambah.php
@@ -64,29 +64,31 @@ if(isset($_POST['input'])) {
 
 ?>
 
-<div class="container">
-	<a class="btn btn-success" href="home.php?menu=kategori"><i class="icon icon-chevron-left"></i> Lihat Data</a>
-		<hr />
-			<h4 class="text-info"><i class="icon icon-pencil"></i> Input Kategori Baru</h4>
-				<form method="post" action="" name="input" class="orange">	
-					<table class="table table-striped">
-						<tr>
-								<td>Nama Kategori</td>
-								<td>:</td>
-								<td><input type="text" name="nm_kategori" class="input-xlarge" maxlength="30"></td>
-								</tr>
-						<tr>
-								<td>Deskripsi</td>
-								<td>:</td>
-								<td><input type="text" name="deskripsi" class="input-xlarge" maxlength="220"></td>
-								</tr>
-						<tr>
-								<td></td>
-								<td colspan="2">
-								<input type="submit" class="btn btn-success" name="input" value="Tambah Kategori">
-								<a class="btn btn-danger" href="home.php?menu=kategori">Batal</a>
-								</td>
-								</tr>
-					</table>
-				</form>
-			</div>
+<div class="row">
+	<div class="span9">
+		<a class="btn btn-success" href="home.php?menu=kategori"><i class="icon icon-chevron-left"></i> Lihat Data</a>
+			<hr />
+				<h4 class="text-info"><i class="icon icon-pencil"></i> Input Kategori Baru</h4>
+					<form method="post" action="" name="input" class="orange">	
+						<table class="table table-striped">
+							<tr>
+									<td>Nama Kategori</td>
+									<td>:</td>
+									<td><input type="text" name="nm_kategori" class="input-xlarge" maxlength="30"></td>
+									</tr>
+							<tr>
+									<td>Deskripsi</td>
+									<td>:</td>
+									<td><input type="text" name="deskripsi" class="input-xlarge" maxlength="220"></td>
+									</tr>
+							<tr>
+									<td></td>
+									<td colspan="2">
+									<input type="submit" class="btn btn-success" name="input" value="Tambah Kategori">
+									<a class="btn btn-danger" href="home.php?menu=kategori">Batal</a>
+									</td>
+									</tr>
+						</table>
+					</form>
+				</div>
+</div>
