db4756c815663bcf88b7e3b0d4ff3c19da86bf3b
Muliana Insan Wahyudi <30404686+muliana98@users.noreply.github.com>
2018-08-18 01:54:37 +0700

    Update tentang-tambah.php

--- admin/content/tentang/tentang-tambah.php
+++ admin/content/tentang/tentang-tambah.php
@@ -34,43 +34,45 @@ if(isset($_POST['input'])) {
 
 ?>
 
-<div class="container">
-	<a class="btn btn-success" href="home.php?menu=tentang"><i class="icon icon-chevron-left"></i> Lihat Data</a>
-		<hr />
-			<h4 class="text-info"><i class="icon icon-pencil"></i> Input Data Tentang</h4>
-				<form method="post" action="" name="input" class="orange" enctype="multipart/form-data">	
-					<table class="table">
-						<tr>
-								<td>Nama</td>
-								<td>:</td>
-								<td><input type="text" name="nm_tentang" class="input-xlarge" maxlength="35"></td>
-								</tr>
-						<tr>
-								<td>Link</td>
-								<td>:</td>
-								<td><input type="text" name="link" class="input-xlarge" maxlength="50"></td>
-								</tr>
-						<tr>
-								<td>Gambar</td>
-								<td>:</td>
-								<td><input type="file" name="gambar" class="input-xlarge" maxlength="60"></td>
-								</tr>
-						<tr>
-						
-								<td>Ket.</td>
-								<td>:</td>
-								<td><textarea name="ket" class="input-xlarge"></textarea></td>
-								</tr>
-						<tr>
-								<td></td>
-								<td colspan="2">
-								<input type="submit" name="input" class="btn btn-success" value="Tambah Tentang">
-								<a class="btn btn-danger" href="home.php?menu=tentang">Batal</a>
-								</td>
-								</tr>
-					</table>
-				</form>
-			</div>
+<div class="row">
+	<div class="span9">
+		<a class="btn btn-success" href="home.php?menu=tentang"><i class="icon icon-chevron-left"></i> Lihat Data</a>
+			<hr />
+				<h4 class="text-info"><i class="icon icon-pencil"></i> Input Data Tentang</h4>
+					<form method="post" action="" name="input" class="orange" enctype="multipart/form-data">	
+						<table class="table">
+							<tr>
+									<td>Nama</td>
+									<td>:</td>
+									<td><input type="text" name="nm_tentang" class="input-xlarge" maxlength="35"></td>
+									</tr>
+							<tr>
+									<td>Link</td>
+									<td>:</td>
+									<td><input type="text" name="link" class="input-xlarge" maxlength="50"></td>
+									</tr>
+							<tr>
+									<td>Gambar</td>
+									<td>:</td>
+									<td><input type="file" name="gambar" class="input-xlarge" maxlength="60"></td>
+									</tr>
+							<tr>
+							
+									<td>Ket.</td>
+									<td>:</td>
+									<td><textarea name="ket" class="input-xlarge"></textarea></td>
+									</tr>
+							<tr>
+									<td></td>
+									<td colspan="2">
+									<input type="submit" name="input" class="btn btn-success" value="Tambah Tentang">
+									<a class="btn btn-danger" href="home.php?menu=tentang">Batal</a>
+									</td>
+									</tr>
+						</table>
+					</form>
+				</div>
+</div>
 								
 								
 								
