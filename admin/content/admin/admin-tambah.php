f79a756bea356ced8739603753fe34a8d9824189
Muliana Insan Wahyudi <30404686+muliana98@users.noreply.github.com>
2018-08-18 00:39:53 +0700

    Update admin-tambah.php

--- admin/content/admin/admin-tambah.php
+++ admin/content/admin/admin-tambah.php
@@ -53,14 +53,25 @@ if(isset($_POST['input'])) {
 	
 	$selectuser = "SELECT username FROM admin WHERE username='$username'";
 	$sqluser = mysqli_query($koneksi, $selectuser);
-	$cek = mysqli_num_rows($sqluser);
+	$cek1 = mysqli_fetch_array($sqluser);
+		$cek_data_username = mysqli_real_escape_string($koneksi, trim($cek1['username']));
 	
-		if($cek==0) {
+	
+	
+	$selectemail = "SELECT email FROM admin WHERE email='$email'";
+	$sqlemail = mysqli_query($koneksi, $selectemail);
+	$cek2 = mysqli_fetch_array($sqlemail);
+		$cek_data_email = mysqli_real_escape_string($koneksi, trim($cek2['email']));
+	
+	
+	
+	
+		if(($cek_data_username != $username) && ($cek_data_email != $email) > 0 ) {
 			
 			$insertquery = "INSERT INTO admin VALUES ('', '$username', '$email', '$nama_lengkap', '$password', '$ket', '', '')";
-			$sql = mysqli_query($koneksi, $insertquery);
+			$sql_insert = mysqli_query($koneksi, $insertquery);
 			
-				if($sql) {
+				if($sql_insert) {
 					
 					echo "<h4><font color='blue'>Data berhasil ditambahkan</font></h4>";
 					
@@ -72,62 +83,86 @@ if(isset($_POST['input'])) {
 				}
 
 		}		
-		else {
+		else if(($cek_data_username == $username) && ($cek_data_email == $email) > 0) {
 				
-				echo "<h5>Maaf, username tidak tersedia. Silakan gunakan username lainnya</h5>";
+				echo "<div class='alert alert-danger'>
+							<button type='button' class='close' data-dismiss='alert'>&times;</button>
+							<strong>Maaf, Username maupun E-Mail tidak tersedia. Silakan gunakan Username dan E-Mail lainnya</strong>
+						</div>";
 				
 			}
+		else if(($cek_data_username == $username)) {
+				
+				echo "<div class='alert alert-danger'>
+							<button type='button' class='close' data-dismiss='alert'>&times;</button>
+							<strong>Maaf, Username tidak tersedia. Silakan gunakan Username lainnya</strong>
+						</div>";
+				
+			}
+		else if(($cek_data_email == $email)) {
+				
+				echo "<div class='alert alert-danger'>
+							<button type='button' class='close' data-dismiss='alert'>&times;</button>
+							<strong>Maaf, E-Mail tidak tersedia. Silakan gunakan E-Mail lainnya</strong>
+						</div";
+				
+			}
+
 		
 		}
 		
 	}
+	
 
 ?>
 
-<div class="container">
-	<a href="home.php?menu=admin" class="btn btn-success"><i class="icon icon-chevron-left"></i> Lihat Data</a>
-	<hr />
-		<h4 class="text-info"><i class="icon icon-pencil"></i> Input Data Anggota</h4>
-			<form method="post" action="" name="input" class="orange">
-				<table class="table table-striped">
-					<tr>
-							<td>Username</td>
-							<td>:</td>
-							<td><input type="text" name="username" class="input-xlarge" maxlength="30"></td>
-							</tr>
-					<tr>
-							<td>Nama Lengkap</td>
-							<td>:</td>
-							<td><input type="text" name="nama_lengkap" class="input-xlarge" maxlength="45"></td>
-							</tr>
-					<tr>
-							<td>E-Mail</td>
-							<td>:</td>
-							<td><input type="text" name="email" class="input-xlarge" maxlength="40"></td>
-							</tr>
-					<tr>
-							<td>Password</td>
-							<td>:</td>
-							<td><input type="password" name="password" class="input-xlarge" maxlength="30"></td>
-							</tr>
-					<tr>
-							<td>Keterangan</td>
-							<td>:</td>
-							<td>
-								<select class="input-xlarge" name="ket">
-								<option value="admin">Admin</option>
-								<option value="moderator">Moderator</option>
-								<option value="user">User</option>
-								</select>
-							</td></tr>
-					<tr>
-							<td></td>
-							<td colspan="2"><input type="submit" name="input" class="btn btn-success" value="Tambahkan Data">
-							<a class="btn btn-warning" href="home.php?menu=admin">Batal</a>
-							</tr>
-					</table>
-			</form>
-		</div>
+
+<div class="row">
+	<div class="span9">
+		<a href="home.php?menu=admin" class="btn btn-success"><i class="icon icon-chevron-left"></i> Lihat Data</a>
+		<hr />
+			<h4 class="text-info"><i class="icon icon-pencil"></i> Input Data Anggota</h4>
+				<form method="post" action="" name="input" class="orange">
+					<table class="table table-striped">
+						<tr>
+								<td>Username</td>
+								<td>:</td>
+								<td><input type="text" name="username" class="input-xlarge" maxlength="30"></td>
+								</tr>
+						<tr>
+								<td>Nama Lengkap</td>
+								<td>:</td>
+								<td><input type="text" name="nama_lengkap" class="input-xlarge" maxlength="45"></td>
+								</tr>
+						<tr>
+								<td>E-Mail</td>
+								<td>:</td>
+								<td><input type="text" name="email" class="input-xlarge" maxlength="40"></td>
+								</tr>
+						<tr>
+								<td>Password</td>
+								<td>:</td>
+								<td><input type="password" name="password" class="input-xlarge" maxlength="30"></td>
+								</tr>
+						<tr>
+								<td>Keterangan</td>
+								<td>:</td>
+								<td>
+									<select class="input-xlarge" name="ket">
+									<option value="admin">Admin</option>
+									<option value="moderator">Moderator</option>
+									<option value="user">User</option>
+									</select>
+								</td></tr>
+						<tr>
+								<td></td>
+								<td colspan="2"><input type="submit" name="input" class="btn btn-success" value="Tambahkan Data">
+								<a class="btn btn-warning" href="home.php?menu=admin">Batal</a>
+								</tr>
+						</table>
+				</form>
+			</div>
+</div>
 
 
 
@@ -138,7 +173,7 @@ if(isset($_POST['input'])) {
 }
 else {
 	
-	echo "<h2>Halaman ini hanya diperuntukan untuk Administrator</h2>";
+	echo "<h2Maaf,<br /> Halaman ini hanya diperuntukan untuk Administrator</h2>";
 	
 }	
 	
