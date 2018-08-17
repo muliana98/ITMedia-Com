61864fdbb13bbc95d656060fa8abe66dcce14c36
Muliana Insan Wahyudi <30404686+muliana98@users.noreply.github.com>
2018-08-18 01:37:52 +0700

    Update profil-foto-ubah.php

--- admin/content/profil/profil-foto-ubah.php
+++ admin/content/profil/profil-foto-ubah.php
@@ -23,7 +23,7 @@ if(isset($_POST['input'])) {
 	move_uploaded_file($sumber, $tujuan);
 	
 	$ambil_foto_profil = mysqli_real_escape_string($koneksi, trim(stripslashes($_REQUEST['ambil_foto_profil'])));
-	$ambil_foto_profil = "../images/profil/".$ambil_foto_profil;
+	$ambil_foto_profil1 = "../images/profil/".$ambil_foto_profil;
 	
 	if(empty($foto_profil)) {
 		
@@ -33,61 +33,82 @@ if(isset($_POST['input'])) {
 					</div> ";
 		
 	}
+	
 
 	else {
 		
-	if(file_exists($ambil_foto_profil)) {
-		unlink($ambil_foto_profil);
-	}
-		
-		$update = "UPDATE admin SET foto_profil='$foto_profil' WHERE id='$id'";
-		$sql = mysqli_query($koneksi, $update);
-		
-			if($sql) echo "<div class='alert alert-info'>
-								<a href='home.php?menu=profil' class='close' data-dismiss='alert'>&times;</a>
-									<strong><i class='icon icon-ok-circle'></i> Foto Profil berhasil diubah!</strong>
-							</div>
-							<meta http-equiv='refresh' content='0.1'>";
-							
-			else echo "<div class='alert alert-danger'>
-								<button type='button' class='close' data-dismiss='alert'>&times;</button>
-									<strong><i class='icon icon-info-sign'></i> Foto Profil gagal diubah!</strong>
-							</div> ";
-					
-	}		
-
+		if(file_exists($ambil_foto_profil1 == $tujuan)) {
+			//unlink($ambil_foto_profil);
 			
+			$huruf_acak = rand(1, 10000).round(microtime(true))."_".$foto_profil ;
+			$update = "UPDATE admin SET foto_profil='$huruf_acak' WHERE id='$id'";
+			$sql = mysqli_query($koneksi, $update);
+			
+				if($sql) echo "<div class='alert alert-info'>
+									<a href='home.php?menu=profil' class='close' data-dismiss='alert'>&times;</a>
+										<strong><i class='icon icon-ok-circle'></i> Foto Profil berhasil diubah!</strong>
+								</div>
+								<meta http-equiv='refresh' content='0.1'>";
+								
+				else echo "<div class='alert alert-danger'>
+									<button type='button' class='close' data-dismiss='alert'>&times;</button>
+										<strong><i class='icon icon-info-sign'></i> Foto Profil gagal diubah!</strong>
+								</div> ";
+						
+		}
+		else {
+			$update = "UPDATE admin SET foto_profil='$foto_profil' WHERE id='$id'";
+			$sql = mysqli_query($koneksi, $update);
+			
+				if($sql) echo "<div class='alert alert-info'>
+									<a href='home.php?menu=profil' class='close' data-dismiss='alert'>&times;</a>
+										<strong><i class='icon icon-ok-circle'></i> Foto Profil berhasil diubah!</strong>
+								</div>
+								<meta http-equiv='refresh' content='0.1'>";
+								
+				else echo "<div class='alert alert-danger'>
+									<button type='button' class='close' data-dismiss='alert'>&times;</button>
+										<strong><i class='icon icon-info-sign'></i> Foto Profil gagal diubah!</strong>
+								</div> ";
+					
+		}
+		
+	}
+
 }
+
 
 ?>
 
-<div class="container">
-	<h4 class="text-info"><i class="icon icon-picture"></i> Perbarui Foto Profil</h4>
-		<form method="post" name="input" action="" class="orange" enctype="multipart/form-data">
-			<input type="hidden" name="id" value="<?php echo $id; ?>" />	
-			<input type="hidden" name="ambil_foto_profil" value="<?php echo $foto_profil_semula; ?>" />
-				<table class="table table-striped">
-					<tr>
-							<td>Foto Profil</td>
-							<td>:</td>
-							<td><input type="file" name="foto_profil" class="input-xlarge" maxlength="120"></td>
-							</tr>
-					<tr>
-							<td></td>
-							<td colspan="2">
-							<input type="submit" name="input" class="btn btn-success" value="Ubah Foto Profil">
-							<a href="home.php?menu=profil" class="btn btn-warning">Batal</a>
-							</td>
-							</tr>
-					<tr>
-							<td colspan="3"></td>
-							</tr>
-					<tr>
-							<td>Hapus Foto Profil</td>
-							<td>:</td>
-							<td><a onclick="return confirm('Anda yakin ingin menghapus Foto Profil?')" href="content/profil/profil-foto-hapus.php?id=<?php echo $id; ?>" class="btn btn-danger"><i class="icon-white icon-trash"></i> Hapus Foto Profil</a></td>
-							</tr>
-							
-			</table>
-		</form>
-	</div>
+<div class="row">
+	<div class="span9">
+		<h4 class="text-info"><i class="icon icon-picture"></i> Perbarui Foto Profil</h4>
+			<form method="post" name="input" action="" class="orange" enctype="multipart/form-data">
+				<input type="hidden" name="id" value="<?php echo $id; ?>" />	
+				<input type="hidden" name="ambil_foto_profil" value="<?php echo $foto_profil_semula; ?>" />
+					<table class="table table-striped">
+						<tr>
+								<td>Foto Profil</td>
+								<td>:</td>
+								<td><input type="file" name="foto_profil" class="input-xlarge" maxlength="120"></td>
+								</tr>
+						<tr>
+								<td></td>
+								<td colspan="2">
+								<input type="submit" name="input" class="btn btn-success" value="Ubah Foto Profil">
+								<a href="home.php?menu=profil" class="btn btn-warning">Batal</a>
+								</td>
+								</tr>
+						<tr>
+								<td colspan="3"></td>
+								</tr>
+						<tr>
+								<td>Hapus Foto Profil</td>
+								<td>:</td>
+								<td><a onclick="return confirm('Anda yakin ingin menghapus Foto Profil?')" href="content/profil/profil-foto-hapus.php?id=<?php echo $id; ?>" class="btn btn-danger"><i class="icon-white icon-trash"></i> Hapus Foto Profil</a></td>
+								</tr>
+								
+				</table>
+			</form>
+		</div>
+</div>
