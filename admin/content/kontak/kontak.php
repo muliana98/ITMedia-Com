ba2c82906f86e06f56f3fe219c79a5c789d413f8
Muliana Insan Wahyudi <30404686+muliana98@users.noreply.github.com>
2018-08-18 01:29:42 +0700

    Update kontak.php

--- admin/content/kontak/kontak.php
+++ admin/content/kontak/kontak.php
@@ -16,6 +16,12 @@
 				
 <?php
 include "koneksi.php";
+include "/../fungsi_admin.php";
+
+$id_akun = mysqli_real_escape_string($koneksi, trim($_SESSION['admin']));
+
+
+
 $dataPerPage = 5;
 if(isset($_GET['page'])) {
 	
@@ -37,7 +43,26 @@ while($hasil = mysqli_fetch_array($sql)) {
 	$email = mysqli_real_escape_string($koneksi, trim(stripslashes($hasil['email'])));
 	$ket = mysqli_real_escape_string($koneksi, trim(stripslashes($hasil['ket'])));
 	
-	echo "<tr><td>$id</td><td>$nama</td><td>$alamat</td><td>$jenis</td><td>$telp</td>
+	if(($id == "1") && ($id_akun == "1")  ) {
+		
+		echo "<tr><td>$id</td><td>$nama</td><td>$alamat</td><td>$jenis</td><td>$telp</td>
+		<td>$email</td>
+		<td>$ket</td>".
+		"<td><a class='btn' href='home.php?menu=kontak-update&&id=$id'><i class='icon icon-edit'></i> Edit</a></td>";
+		
+		
+	?>
+	
+
+	<td><a onclick="return confirm('Anda yakin ingin menghapus Data Kontak: <?php echo $nama; ?>   ?')" class="btn btn-danger" href="content/kontak/kontak-hapus.php?id=<?php echo $id; ?>"><i class="icon-white icon-trash"></i> Hapus</a></td></tr>
+
+<?php	
+	}
+
+	
+	else if($id != "1") {
+		
+		echo "<tr><td>$id</td><td>$nama</td><td>$alamat</td><td>$jenis</td><td>$telp</td>
 		<td>$email</td>
 		<td>$ket</td>".
 		"<td><a class='btn' href='home.php?menu=kontak-update&&id=$id'><i class='icon icon-edit'></i> Edit</a></td>";
@@ -48,8 +73,14 @@ while($hasil = mysqli_fetch_array($sql)) {
 	
 	<td><a onclick="return confirm('Anda yakin ingin menghapus Data Kontak: <?php echo $nama; ?>   ?')" class="btn btn-danger" href="content/kontak/kontak-hapus.php?id=<?php echo $id; ?>"><i class="icon-white icon-trash"></i> Hapus</a></td></tr>
 	
+		
 	
-<?php	
+	
+
+	
+<?php
+	}
+	
 }
 echo "</table>";
 echo "<div class='container'>";
@@ -60,37 +91,12 @@ $data = mysqli_fetch_array($result);
 $jumData = mysqli_real_escape_string($koneksi, $data['jumData']);
 $jumPage = ceil($jumData/$dataPerPage);
 
-$showPage = "";
+$showPage = $jumPage;
 
-echo "<div class='td_pagination style3 col-sm-12'><div class='inner'>";
-if($noPage>1) {
-	
-	echo "<a class='page_prev btn btn-success' href='home.php?menu=kontak&&page=".($noPage-1)."'><span><</span></a>";
-	
-}
-for($page = 1; $page <= $jumPage; $page++) {
-	
-	if((($page >= $noPage - 3)) && ($page <= $noPage + 3) || ($page == 1) || ($page == $jumPage)) {
-		
-		if(($showPage == 1) && ($page != 2)) echo "..";
-		if(($showPage != ($jumPage - 1)) && ($page == $jumPage)) echo "..";
-		if($page == $noPage) {
-			
-			echo "<a href='#' class='page-numbers btn btn-warning'><b>"
-				.$page."</b></a>";
-			
-		}
-		$showPage = $page;
-	}
-	
-}
-if($noPage < $jumPage) {
-	
-	echo "<a class='page_next btn btn-success' href='home.php?menu=kontak&&page=".($noPage+1)."'><span>></span></a></div></div>";
-	
-}
-echo "</div>";
-echo "<h5>Total Record : $jumData records</h5>";
+$halaman_url = "home.php?menu=kontak&&page=";
+
+
+echo paginasi($halaman_url, $jumData, $jumPage, $noPage, $showPage);
 
 
 ?>
